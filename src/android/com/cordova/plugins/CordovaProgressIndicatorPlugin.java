c0577e8aefd81b88069aa35ecb06bb6f19368ef7
Tomas Radvansky <tomas.radvansky@6pmplc.com>
2017-02-16 15:14:22 +0800

    Typo plugin.xml Android Added show/hide methods

--- src/android/com/cordova/plugins/CordovaProgressIndicatorPlugin.java
+++ src/android/com/cordova/plugins/CordovaProgressIndicatorPlugin.java
@@ -7,48 +7,53 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.PluginResult.Status;
-import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
-
+import android.graphics.Color;
 import android.util.Log;
 
-import java.util.Date;
+
 import cc.cloudist.acplibrary.ACProgressFlower;
 import cc.cloudist.acplibrary.ACProgressConstant;
 
 public class CordovaProgressIndicatorPlugin extends CordovaPlugin {
-  private static final String TAG = "CordovaProgressIndicatorPlugin";
-  private ACProgressFlower dialog;
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+    private static final String TAG = "ProgressIndicatorPlugin";
+    private ACProgressFlower dialog;
+    private CordovaInterface cordova;
 
-    Log.d(TAG, "Initializing CordovaProgressIndicatorPlugin");
-  }
-
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-
-    if(action.equals("show")) {
-      String phrase = args.getString(0);
-      if (dialog != null)
-      {
-        //Hide existing dialog
-      }
-      dialog = new ACProgressFlower.Builder(this)
-                              .direction(ACProgressConstant.DIRECT_CLOCKWISE)
-                              .themeColor(Color.WHITE)
-                              .text("Title is here)
-                              .fadeColor(Color.DKGRAY).build();
-      dialog.setCanceledOnTouchOutside(false);
-      dialog.show();
-
-    } else if(action.equals("hide")) {
-      // An example of returning data back to the web layer
-      final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
-      callbackContext.sendPluginResult(result);
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        this.cordova = cordova;
+        Log.d(TAG, "Initializing CordovaProgressIndicatorPlugin");
     }
-    return true;
-  }
+
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("show")) {
+            String message = args.getString(0);
+            if (dialog != null) {
+                //Hide existing dialog
+                dialog.dismiss();
+            }
+            dialog = new ACProgressFlower.Builder(this.cordova.getActivity())
+                    .direction(ACProgressConstant.DIRECT_CLOCKWISE)
+                    .themeColor(Color.WHITE)
+                    .text(message)
+                    .fadeColor(Color.DKGRAY).build();
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+            final PluginResult result = new PluginResult(PluginResult.Status.OK);
+            callbackContext.sendPluginResult(result);
+        } else if (action.equals("hide")) {
+            if (dialog != null) {
+                //Hide existing dialog
+                dialog.dismiss();
+                dialog = null;
+            }
+            final PluginResult result = new PluginResult(PluginResult.Status.OK);
+            callbackContext.sendPluginResult(result);
+        }
+        return true;
+    }
 
 }
